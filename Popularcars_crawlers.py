1811ec6341ab223cbb8a64b0a89950caa3a68fe4
YuandZhang <1055755430@qq.com>
2021-10-05 10:14:35 +0800

    提交

--- Popularcars_crawlers.py
+++ Popularcars_crawlers.py
@@ -4,3 +4,16 @@
 # @Site    : Shenyang
 # @File    : Popularcars_crawlers.py
 # @Software: PyCharm
+#热门车型爬虫
+from selenium import  webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver=webdriver.Chrome()
+driver.get("https://www.dongchedi.com/auto/library/x-x-x-x-x-x-x-x-x-x-x")
+#将滚动条移动到页面的底部
+import time
+driver.execute_script("window.scrollBy(0,3000)")
+time.sleep(1)
+
+
+
